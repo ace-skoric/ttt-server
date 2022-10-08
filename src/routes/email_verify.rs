c2efc7312d10c510754225895bdfcb8566d1ed0f
Aleksandar Skoric <djedo.official@gmail.com>
2022-10-08 02:31:22 +0200

    Changed verification path

--- src/routes/email_verify.rs
+++ src/routes/email_verify.rs
@@ -1,5 +1,6 @@
 use actix_session::Session;
 use actix_web::{get, post, web, HttpResponse};
+use ttt_db::TttDbErr;
 use ttt_mailer::SendVerificationEmail;
 use uuid::Uuid;
 
@@ -7,17 +8,21 @@ use crate::util::SessionData;
 use crate::util::TttApiErr;
 use crate::AppState;
 
-#[post("/verify/{uuid}")]
+#[post("/verify/uuid/{uuid}")]
 async fn verify(
     data: web::Data<AppState>,
-    uuid: web::Path<Uuid>,
+    uuid: web::Path<String>,
 ) -> Result<HttpResponse, TttApiErr> {
+    let uuid = match Uuid::parse_str(&uuid) {
+        Ok(uuid) => uuid,
+        Err(_) => return Err(TttApiErr::from(TttDbErr::EmailVerifyNotFound)),
+    };
     let db = &data.ttt_db;
     let mailer = data.mail_worker.clone();
     let f = move |username: String, email: String, uuid: Uuid| {
         mailer.do_send(SendVerificationEmail::new(username, email, uuid))
     };
-    db.verify_email(*uuid, f).await?;
+    db.verify_email(uuid, f).await?;
     Ok(HttpResponse::Created().json("Welcome to Crystalium"))
 }
 
